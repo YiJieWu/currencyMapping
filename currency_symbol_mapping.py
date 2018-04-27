2e156fec417758ea5076c8e552b80d1020171125
Yijie Wu <yijiewuboilerup@gmail.com>
2018-04-27 13:48:47 -0500

    first commit

---
+++ currency_symbol_mapping.py
@@ -0,0 +1,10 @@
+currency_mapping_table = {
+
+                          'ARS': '\u0024'
+                          'AUD': '\u0024'
+                          'BOB': '$b'
+                          'CNY': '\uffe5',
+                          'EUR': '\u20ac',
+                          'GBP': '\u00a3',
+                          'USD': '\u0024'
+                          }
